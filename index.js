d34d4e63a15e9eb5941bdfa2da269a8ad6a42f81
jhhest <janvanhest@outlook.com>
2020-02-14 11:25:12 +0100

    keeping up with david.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const messageRouter = require(
+  './message/router'
+)
 
-const app = express()
+const app = express ()
 
 const port = 4000
 
-function onListen() {
-console.log(`listening on:${port}`)
+function onListen () {
+  console.log(`Listening on :${port}`)
 }
+
+const jsonMiddleware = express
+  .json()
+app.use(jsonMiddleware)
+
+app.use(messageRouter)
 
 app.listen(port, onListen)
---
+++ message/model.js
@@ -0,0 +1,11 @@
+const db = require('../db')
+const Sequelize = require('sequelize')
+
+const Message = db.define(
+  'message',
+  {
+    text: Sequelize.STRING
+  }
+)
+
+module.exports = Message
