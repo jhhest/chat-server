4eea3d3f37c57f22a588138a0af4d3dde9defc70
jhhest <janvanhest@outlook.com>
2020-02-14 11:58:42 +0100

    added stream with Sse

--- message/router.js
+++ message/router.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const Message = require("./model");
+const Sse = require("json-sse");
+
+const stream = new Sse(); // is going to be a list that we are going to send to.
 
 const { Router } = express;
 
 const router = Router();
+
+// get on the stream
+router.get("/stream", (request, response, next) => {
+  stream.updateInit('test'); // prepare this data, so when the next client connects, you send it over the streams and immidiately connect the client.
+  stream.init(request, response);
+});
+
+router.get("/message", async function(request, response) {
+  try {
+    const messages = await Message.findAll();
+    response.send(messages);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post("/message", async function(request, response, next) {
   try {
